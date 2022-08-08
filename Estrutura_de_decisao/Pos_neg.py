5b5908bf1a3723414c448e2f973e8d051838085b
Renatofeliz <renato_felizardoo@hotmail.com>
2022-08-08 17:37:39 -0300

    PosNeg, VogalCons

---
+++ Estrutura_de_decisao/Pos_neg.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um numero: '))
+
+if num < 0:
+    print('Numero negativo: {}.'.format(num))
+else:
+    if num >= 0:
+        print('Numero positivo: {}.'.format(num))
+     
---
+++ Estrutura_de_decisao/Vogal_Consoante.py
@@ -0,0 +1,8 @@
+letra = str(input('Digite uma letra para saber se é vogal ou consoante!: '))
+
+if letra in 'Aa, Ee, Ii, Oo, Uu':
+    print('Letra é uma vogal! {}'.format(letra))
+else:
+    print('Letra é uma consoante! {}'.format(letra))
+        
+
